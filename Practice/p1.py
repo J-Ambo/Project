532c1e31949d666a7ee7e407efe98da27a7a1fcf
JAC <jamescrawley19@btinternet.com>
2024-08-13 14:15:29 +0100

    Update boundary conditions (added environment method)

--- Practice/p1.py
+++ Practice/p1.py
@@ -12,11 +12,11 @@ class Bird:
         self.dir = np.linalg.norm(np.array([random.uniform(-1, 1), random.uniform(-1, 1)]))  # Direction of the bird
         self.speed = 1                     # Speed of the bird
 
-        self.cohesion_factor = 0.05
-        self.separation_factor = 0.05
-        self.alignment_factor = 0.05
-        self.sep_distance = 30
-        self.neighbourhood = 30
+        self.cohesion_factor = 0.02
+        self.separation_factor = 0.01
+        self.alignment_factor = 0.01
+        #self.sep_distance = 30
+        self.neighbourhood = 20
         
 
 
@@ -37,7 +37,7 @@ class Bird:
                 align_vector += bird.dir
                 avg_pos += bird.pos
 
-                separation_vector += (self.pos - bird.pos)/(self.distance(bird))**2
+                separation_vector += (self.pos - bird.pos)/(self.distance(bird))
 
 
                 #if self.distance(bird) <= self.sep_distance and bird != self:
@@ -55,30 +55,19 @@ class Bird:
             align_vector /= num_neighbours
             avg_pos /= num_neighbours
             separation_vector /= num_neighbours
-    
+
+        else:
+            pass
 
         cohesion_vector = avg_pos - self.pos
 
         # Update the bird's direction based on the average direction of nearby birds
         self.dir += (align_vector * self.alignment_factor) + (cohesion_vector * self.cohesion_factor) + (separation_vector * self.separation_factor)
         self.dir /= np.linalg.norm(self.dir)
-
-
-
-        # Update the bird's position
-        #if (self.pos[0], self.pos[1]) >= (100, 100) or (self.pos[0], self.pos[1]) <= (0, 0):  #boundary conditions (if a bird reaches the boundary it will turn around)
-            #self.dir = -self.dir
-            #self.pos = self.dir * self.speed
-
-        if self.pos[0] <= env.left_limit or self.pos[0] >= env.right_limit:
-            self.dir[0] = -self.dir[0]
-
-        if self.pos[1] <= env.bottom_limit or self.pos[1] >= env.top_limit:
-            self.dir[1] = -self.dir[1]
         
         self.pos += self.dir * self.speed
         #self.pos = np.clip(self.pos, 0, 100)  #boundary conditions (if a bird reaches the boundary it will turn around)
-        #self.pos %= 3
+        
 
     def distance(self, other_bird):
         return ((self.pos[0] - other_bird.pos[0]) ** 2 + (self.pos[1] - other_bird.pos[1]) ** 2) ** 0.5
@@ -94,15 +83,24 @@ class Environment:
         self.right_limit = width
         self.top_limit = height 
 
-        #self.left_border = np.array([0, height])
-        #self.right_border = np.array([width, 0]) + np.array([0, height])
-        #self.top_border = np.array([])
-        #self.bottom_border =
+    def avoid_env(self, other_bird):
+        if other_bird.pos[0] <= self.left_limit or other_bird.pos[0] >= self.right_limit:
+            other_bird.dir[0] = -other_bird.dir[0]
+
+        elif other_bird.pos[1] <= self.bottom_limit or other_bird.pos[1] >= self.top_limit:
+            other_bird.dir[1] = -other_bird.dir[1]
+
+        else:
+            pass
+
+        other_bird.pos += other_bird.dir * other_bird.speed
 
     
-    
+    def obstacle(self, other_bird):
+        if other_bird.pos[0] >= 40 and other_bird.pos[0] <= 60 and other_bird.pos[1] >= 40 and other_bird.pos[1] <= 60:
+            other_bird.dir = -other_bird.dir
+            other_bird.pos += other_bird.dir * other_bird.speed
 
-            
 
 
 
@@ -112,7 +110,7 @@ env = Environment(100,100)
 
 #Create a list of birds
 birds = []
-for _ in range(20):
+for _ in range(100):
     x = random.uniform(5, 95)
     y = random.uniform(5, 95)
     birds.append(Bird(x, y))
@@ -134,13 +132,16 @@ scatt = ax1.scatter([bird.pos[0] for bird in birds], [bird.pos[1] for bird in bi
 def update_frames(frame):
     for bird in birds:
         bird.update(birds)
+        env.avoid_env(bird)
+        #env.obstacle(bird)
 
     # Update the scatter plot data
     scatt.set_offsets([(bird.pos[0], bird.pos[1]) for bird in birds])
     return scatt
 
 # Create the animation
-anim = animation.FuncAnimation(fig1, update_frames, frames=500, interval=50)
+anim = animation.FuncAnimation(fig1, update_frames, frames=500, interval=50, repeat=True)
+
 ax1.plot(np.zeros(100), np.linspace(0, 100, 100), color='black')
 ax1.plot(np.full(100,100), np.linspace(0, 100, 100), color='black')
 ax1.plot(np.linspace(0,100,100), np.zeros(100), color='black')
