2b2e1d75aef40ae092a549dca4b1a56d5507e7ec
JAC <jamescrawley19@btinternet.com>
2024-10-10 17:55:22 +0100

    Added model animation.

--- Polerisation_ABM/model.py
+++ Polerisation_ABM/model.py
@@ -1,7 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import random
-from Polerisation_ABM.model_entities import Environment, Human
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+#import sys
+#sys.path.append('C:/Users/44771/Documents/Level4Project/Polerisation_ABM/model.py')
+from model_entities import Environment, Human
+random.seed(0)
 
 
 env = Environment(100)
@@ -11,21 +15,24 @@ humans = []
 for i in range(100):
     x = random.randint(0,100)
     y = random.randint(0,100)
-    op = random.randint(-1,1)
+    op = random.choice([-1,1])
     human = Human(x, y, op)
     humans.append(human)
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+ax.set_axis_off()
 ax.set_xlim(0, 100)
 ax.set_ylim(0, 100)
 
-scatter = ax.scatter([human.pos[0] for human in humans], [human.pos[1] for human in humans], c=['blue' if human.opinion == 1 else 'red' if human.opinion == -1 else 'green' for human in humans])
 
-#for n in range (100):
-    #for human in humans:
-        #human.move(human.pos[0], human.pos[1])
+scatter = ax.scatter([human.pos[0] for human in humans], [human.pos[1] for human in humans], c=[human.opinion for human in humans], cmap='bwr')
 
-    
-plt.show()
+def update_frame(frame):
+    for human in humans:
+        human.move(human.pos[0], human.pos[1], env)
+    scatter.set_offsets([(human.pos[0], human.pos[1]) for human in humans])
+    return scatter
+ani = animation.FuncAnimation(fig, update_frame, frames=100, interval=500, repeat=True)
+plt.show(block=True)
