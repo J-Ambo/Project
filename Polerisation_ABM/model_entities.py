afea0fd24b53706ddcd1119e5682996e57d6f918
JAC <jamescrawley19@btinternet.com>
2024-10-10 17:54:04 +0100

    Human move(random) function/method added using a helper function 'adjust_coordinate'.

--- Polerisation_ABM/model_entities.py
+++ Polerisation_ABM/model_entities.py
@@ -3,26 +3,34 @@ import random
 
 
 class Environment:
-    def _init_(self, size):
+    def __init__(self, size):
         self.size = size
     
     def create_environment(self):
-        self.environment = np.zeros(self.size, self.size)
+        self.environment = np.zeros((self.size, self.size))
         return self.environment
 
-
 class Human:
-    def _init_(self, x, y, opinion):
+    def __init__(self, x, y, opinion):
         self.pos = np.array([x,y])
         self.opinion = opinion
         #self.identity = 0
 
-    def move(self, x, y):
-        self.newx = Environment.size - (x + random.randint(-1, 1))%Environment.size
-        self.newy = Environment.size - (y + random.randint(-1, 1))%Environment.size
+    def move(self, x, y, env_instance):
+  
+        self.newx = adjust_coordinate(x, env_instance.size)
+        self.newy = adjust_coordinate(y, env_instance.size)
+
         self.pos = np.array([self.newx, self.newy])
         return self.pos
-    
 
+def adjust_coordinate(coord, size):
+    '''Ensures agents do not leave the environment'''
+    if coord >= size:
+        return size - 1
+    elif coord <= 0:
+        return coord + 1
+    else:
+        return coord + random.randint(-1, 1)
 
 
