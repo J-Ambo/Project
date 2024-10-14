e31c1ce43d6b21d9375e926eddec2e798d685b90
JAC <jamescrawley19@btinternet.com>
2024-10-14 14:34:00 +0100

    Added interaction method for Human agents

--- Polerisation_ABM/model_entities.py
+++ Polerisation_ABM/model_entities.py
@@ -11,10 +11,13 @@ class Environment:
         return self.environment
 
 class Human:
-    def __init__(self, x, y, opinion):
+    opinion_threshold = 0.2
+
+    def __init__(self, x, y, opinion, scepticism):
         self.pos = np.array([x,y])
         self.opinion = opinion
-        #self.identity = 0
+        self.scepticism = scepticism
+        #self.influence
 
     def move(self, x, y, env_instance):
   
@@ -24,6 +27,18 @@ class Human:
         self.pos = np.array([self.newx, self.newy])
         return self.pos
 
+    def calculate_distance(self, other_human_instance):
+        return ((self.pos[0] - other_human_instance.pos[0])**2 + (self.pos[1] - other_human_instance.pos[1])**2)**0.5
+
+    def interact(self, other_human_instance):
+        if abs(self.opinion - other_human_instance.opinion) <= Human.opinion_threshold:
+            self.opinion += (1 - self.scepticism) * (other_human_instance.opinion - self.opinion)
+            other_human_instance.opinion += (1 - other_human_instance.scepticism) * (self.opinion - other_human_instance.opinion)
+        else:
+            self.opinion += 0
+            other_human_instance.opinion += 0
+
+
 def adjust_coordinate(coord, size):
     '''Ensures agents do not leave the environment'''
     if coord >= size:
@@ -32,5 +47,4 @@ def adjust_coordinate(coord, size):
         return coord + 1
     else:
         return coord + random.randint(-1, 1)
-
 
