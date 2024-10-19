96bc58e69618b939a643834b9fd35a9427398328
JAC <jamescrawley19@btinternet.com>
2024-10-19 16:48:11 +0100

    Minor changes for compatability with model_visualisation. There are still some discrepancies between the animation and opinion data plot.

--- Polerisation_ABM/model.py
+++ Polerisation_ABM/model.py
@@ -1,13 +1,16 @@
 import random
+import numpy as np
 from Human_agent import Human
 from Environment_agent import Environment
 
 class Model:
-    def __init__(self, population, size):
+    def __init__(self, population, size, time):
         self.population = population
         self.size = size
-        self.humans = []
+        self.frames = time
+        self.humans = np.array([])
         self.env = Environment(size)
+        #self.seed = random.randint(0, 1000)
         self.create_humans()
 
     def create_humans(self):
@@ -15,33 +18,29 @@ class Model:
         for i in range(self.population):
             x = random.randint(0, self.env.size)
             y = random.randint(0, self.env.size)
-            op = random.uniform(-1, 1)
-            sc = random.uniform(0, 1)
+            op = random.uniform(0, 1)
+            sc = 0 #random.uniform(0, 1)
 
             human = Human(x, y, op, sc)
-            self.humans.append(human)
+            self.humans = np.append(self.humans, human)
 
     def update_timestep(self):
         '''Function to update the model each timestep'''
-        for human in self.humans:
-            human.move(human.pos[0], human.pos[1], self.env.size)
+        #for human in self.humans:
+            #human.move(human.pos[0], human.pos[1], self.env.size)
 
         random_index = random.randint(0, self.population)
         random_human = self.humans[random_index - 1]
         random_human.interact(random.choice(self.humans))
+        #print(random_index)
+        #print(self.log_data())
 
     def log_data(self):
         '''Function to log data for each human'''
-        opinion_data = []
+        opinion_data = np.array([])
         for human in self.humans:
-            opinion_data.append(human.opinion)
+            opinion_data = np.append(opinion_data, human.opinion)
         return opinion_data
-
-    def run_model(self, frames):
-        for frame in range(frames):
-            self.update_frame()
-
-        return self.log_data()
     
 
 
