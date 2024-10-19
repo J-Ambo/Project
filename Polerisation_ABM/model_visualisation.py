ea190f4f3d90613255068172d69c81acde8c952c
JAC <jamescrawley19@btinternet.com>
2024-10-19 16:46:14 +0100

    Added visualisation for evolution of average polerisation over time.

--- Polerisation_ABM/model_visualisation.py
+++ Polerisation_ABM/model_visualisation.py
@@ -7,15 +7,33 @@ from Human_agent import Human
 from Environment_agent import Environment
 from model import Model
 
-Human.opinion_threshold = 0.8
+Human.opinion_threshold = 1
 
-model_instance = Model(100, 50)
+model_instance = Model(5, 50, 10)
+print(model_instance.log_data())
+#model_instance.run_model(model_instance.frames)
+
+fig0, ax0 = plt.subplots()
+
+average_opinion_all_frames = np.array([])
+for frame in range(model_instance.frames):
+    model_instance.update_timestep()
+    average_opinion = sum(model_instance.log_data())/model_instance.population
+    average_opinion_all_frames = np.append(average_opinion_all_frames, average_opinion)
+
+    #print(model_instance.log_data())
+    
+ax0.plot(range(model_instance.frames), average_opinion_all_frames)  
+
+
+
+print(model_instance.log_data())
+
 
 fig, ax = plt.subplots()
 ax.set_axis_off()
 ax.set_xlim(0, model_instance.size)
 ax.set_ylim(0, model_instance.size)
-
 scatter = ax.scatter([human.pos[0] for human in model_instance.humans], [human.pos[1] for human in model_instance.humans], c=[human.opinion for human in model_instance.humans], cmap='rainbow',s=[10*np.exp(human.scepticism*2) for human in model_instance.humans])
 
 def update_plot_frame(frame):
@@ -27,7 +45,7 @@ def update_plot_frame(frame):
 
     return scatter
 
-ani = animation.FuncAnimation(fig, update_plot_frame, frames=1000, interval=200, repeat=True)
+ani = animation.FuncAnimation(fig, update_plot_frame, frames=model_instance.frames, interval=200, repeat=True)
 plt.show(block=True)
 
 from IPython.display import HTML
