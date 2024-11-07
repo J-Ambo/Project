3fcf734c72c97b2856060acf57779cca64b85f41
JAC <jamescrawley19@btinternet.com>
2024-11-07 15:08:42 +0000

    Refiled

--- Polerisation_ABM/play_model/Environment_agent.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Environment:
-    def __init__(self, size):
-        self.size = size
-    
-    def create_environment(self):
-        self.environment = np.zeros((self.size, self.size))
-        return self.environment
--- Practice/environment_class.py
+++
@@ -1,3 +0,0 @@
-class Environment:
-    def __init__(self, size):
-        self.size = size
