d76a9c23cbc15bcd20c598619182f7b113c3a8cc
JAC <jamescrawley19@btinternet.com>
2024-11-13 15:50:57 +0000

    Made minor changes to animation files.

--- ProjectFolder/BoidsModel.py
+++ ProjectFolder/BoidsModel.py
@@ -7,7 +7,11 @@ from predator_class import Predator
 from prey_class import Prey
 '''This script is an alternative to the plt.pause method for creating an animation of the model.'''
 
-env = Environment(50)
+POPULATION = 50
+ARENA_RADIUS = 70
+TIMESTEPS = 500
+
+env = Environment(ARENA_RADIUS)
 all_predators = []
 for _ in range(0):
     r = random.uniform(0, env.radius)
@@ -18,7 +22,7 @@ for _ in range(0):
     all_predators.append(Predator(x, y))
 
 all_prey = []
-for _ in range(1):
+for _ in range(POPULATION):
     r = random.uniform(0, env.radius)
     theta = random.uniform(0, 2*np.pi)
 
@@ -26,16 +30,16 @@ for _ in range(1):
     y = r * np.sin(theta)
     all_prey.append(Prey(x, y))
 
-all_birds = all_prey + all_predators
+all_agents = all_prey + all_predators
 
 #Animation using FuncAnimation method
 fig, ax = plt.subplots(figsize=(6,6))
 ax.set_axis_off()
 ax.set_xlim(-env.radius*1.01, env.radius*1.01)
 ax.set_ylim(-env.radius*1.01, env.radius*1.01)
-scatt = ax.scatter([bird.pos[0] for bird in all_birds],
-                    [bird.pos[1] for bird in all_birds],
-                    c=['blue' if isinstance(bird, Prey) else 'red' for bird in all_birds],
+scatt = ax.scatter([agent.position[0] for agent in all_agents],
+                    [agent.position[1] for agent in all_agents],
+                    c=['blue' if isinstance(agent, Prey) else 'red' for agent in all_agents],
                       s=10)
 centre = [0,0]
 radius = env.radius
@@ -47,15 +51,15 @@ ax.plot(x, y, c='black')
 #Update function for the animation
 def update_frames(frame):
     for prey in all_prey:
-        prey.update_prey(all_birds, env)
+        prey.update_prey(all_agents, env)
 
     for predator in all_predators:
-        predator.update_predator(all_birds, env)
+        predator.update_predator(all_agents, env)
 
-    scatt.set_offsets([(bird.pos[0], bird.pos[1]) for bird in all_birds])
+    scatt.set_offsets([(agent.position[0], agent.position[1]) for agent in all_agents])
     return scatt
 
-anim = animation.FuncAnimation(fig, update_frames, frames=500, interval=50, repeat=True)
+anim = animation.FuncAnimation(fig, update_frames, frames=TIMESTEPS, interval=50, repeat=True)
 plt.show()
 
 from IPython.display import HTML
--- ProjectFolder/view.py
+++ ProjectFolder/view.py
@@ -7,28 +7,33 @@ from environment_class import Environment
 '''This script is a alternative to matplotlib.animation.FuncAnimation for creating
  an animation of the model. It uses plt.pause between each iteration to update the plot.'''
 
-POPULATION = 50
-ARENA_RADIUS = 25
+POPULATION = 2
+ARENA_RADIUS = 20
+TIMESTEPS = 500
 
 env = Environment(ARENA_RADIUS)
-agents = []
+all_agents = []
+
 for _ in range(POPULATION):
     r = random.uniform(0, env.radius)
     theta = random.uniform(0, 2*np.pi)
 
-    x = r * np.cos(theta)
-    y = r * np.sin(theta)
-    agents.append(Prey(x, y))
+    x = r/2 * np.cos(theta)
+    y = r/2 * np.sin(theta)
+    all_agents.append(Prey(x, y))
+#print(env.calculate_agents_positions(all_agents))
 
 #Animation using plt.pause method
 fig, ax = plt.subplots(figsize=(6,6))
 ax.set_axis_off()
 ax.set_xlim(-env.radius*1.01, env.radius*1.01)
 ax.set_ylim(-env.radius*1.01, env.radius*1.01)
-scatt = ax.scatter([agent.pos[0] for agent in agents],
-                    [agent.pos[1] for agent in agents],
-                    c=['blue' if isinstance(agent, Prey) else 'red' for agent in agents],
-                      s=10)
+
+scatt = ax.scatter([agent.position[0] for agent in all_agents],
+            [agent.position[1] for agent in all_agents],
+            c=['blue' if isinstance(agent, Prey) else 'red' for agent in all_agents],
+            s=10)
+
 centre = [0,0]
 radius = env.radius
 theta = np.linspace(0, 2*np.pi, 100)
@@ -36,11 +41,14 @@ x = centre[0] + radius * np.cos(theta)
 y = centre[1] + radius * np.sin(theta)
 ax.plot(x, y, c='black')
 
-for _ in range(2000):            #Update the scatter plot for each iteration
-    for agent in agents:
-        agent.update_prey(agents, env)
-    
-    scatt.set_offsets([(agent.pos[0], agent.pos[1]) for agent in agents])
+for _ in range(TIMESTEPS):            #Update the scatter plot for each timestep
+
+    for index, agent in enumerate(all_agents): 
+        agent.update_prey(all_agents, env)
+
+    scatt.set_offsets([(agent.position[0], agent.position[1]) for agent in all_agents])
 
     plt.pause(0.001)
 plt.show()
+
+
