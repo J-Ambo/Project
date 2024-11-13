963591234069ec86e9ce80380f275f3a9be130aa
JAC <jamescrawley19@btinternet.com>
2024-11-13 15:46:23 +0000

    Added method to Environment class which records all agents positions at one instant in time.

--- ProjectFolder/environment_class.py
+++ ProjectFolder/environment_class.py
@@ -1,6 +1,12 @@
-
+import numpy as np
 '''This script contains the Environment class. This class is used to create an environment instance'''
 
 class Environment:
     def __init__(self, size):
         self.radius = size
+
+    def calculate_agent_positions(self, agents):
+        '''This method calculates the positions of all agents in the environment.'''
+        agent_positions = np.empty((0,2))
+        agent_positions = np.append(agent_positions, [agent.position for agent in agents], axis=0)
+        return agent_positions
