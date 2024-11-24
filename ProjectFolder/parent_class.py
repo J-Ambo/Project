1f1b6248584b396da9f847ddc399549526394554
JAC <jamescrawley19@btinternet.com>
2024-11-24 16:06:59 +0000

    Re-evaluating the maximal turning angle as it relates to the minimum turning radius

--- ProjectFolder/parent_class.py
+++ ProjectFolder/parent_class.py
@@ -16,12 +16,15 @@ class Parent:
         self.direction /= np.linalg.norm(self.direction)
         self.previous_direction = self.direction
 
+        self.body_length = 1
         self.speed = np.clip(np.random.normal(loc=0.5, scale=0.1), 0.45, 0.55)     #np.random.choice(np.linspace(0.5, 1, 5))
         self.perception_angle = np.deg2rad(270)
+        self.minimum_turning_radius = 0.2 * self.body_length
+        self.maximal_turning_angle = np.deg2rad(60)  #np.arcsin(self.speed / (2 * self.minimum_turning_radius))
 
         (self.radius_of_repulsion,
         self.radius_of_alignment, 
-        self.radius_of_attraction) = 1, 4, 50
+        self.radius_of_attraction) = 1, 4, 15
         
         (self.neighbours_in_repulsive_zone, 
          self.neighbours_in_alignment_zone, 
