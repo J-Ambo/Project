b88432fb53ca3738da5fcd5f33e44950cb6aa9f3
JAC <jamescrawley19@btinternet.com>
2024-11-30 18:13:58 +0000

    Fixed data recording for repeat runs

--- ProjectFolder/data_class.py
+++ ProjectFolder/data_class.py
@@ -1,18 +1,14 @@
 import numpy as np
 
 class DataRecorder:
-    def __init__(self, population, time, repetitions):
-        self.all_agents = population.population_array
-        self.population = population
-
-        self.data = np.zeros((repetitions, time, len(self.all_agents), 2, self.population.dimension))
+    def __init__(self, population_size, dimensions, time, repetitions):
+        self.data = np.zeros((repetitions, time, population_size, 2, dimensions))
         self.polarisation_data = np.zeros((repetitions, time))
         self.rotation_data = np.zeros((repetitions, time))
 
-        self.data[0][0][:, 0] = [agent.position for agent in self.all_agents]
-        self.data[0][0][:, 1] = [agent.direction for agent in self.all_agents]
-             
-    def update_data(self, time, repetitions):
+    def update_data(self, population, time, repetitions):
+        self.all_agents = population.population_array
+        self.population = population
         self.data[repetitions][time][:, 0] = [agent.position for agent in self.all_agents]
         self.data[repetitions][time][:, 1] = [agent.direction for agent in self.all_agents]
         self.polarisation_data[repetitions][time] = self.population.polarisation
@@ -20,3 +16,13 @@ class DataRecorder:
         
     def get_data(self):
         return self.data, self.polarisation_data, self.rotation_data
+
+'''
+
+from population_class import Population
+from environment_class import Environment
+env = Environment(10, 3)
+pop = Population(4, 2, env)
+data = DataRecorder(pop, 5, 2)
+print(data.get_data())
+'''
--- ProjectFolder/view.py
+++ ProjectFolder/view.py
@@ -9,82 +9,88 @@ from data_class import DataRecorder
 '''This script is a alternative to matplotlib.animation.FuncAnimation for creating
  an animation of the model. It uses plt.pause between each iteration to update the plot.'''
 
-POPULATION = 10
-ARENA_RADIUS = 30
-TIMESTEPS = 100
+POPULATION = 50
+NEIGHBOURS = 20
+ARENA_RADIUS = 40
+TIMESTEPS = 500
 DIMENSIONS = 3
-REPETITIONS = 1
+REPETITIONS = 10
+
+
+graph_on = False
 
 env = Environment(ARENA_RADIUS, DIMENSIONS)
+data_recorder = DataRecorder(POPULATION, DIMENSIONS, TIMESTEPS, REPETITIONS)
 
 
-#Animation using plt.pause method
-fig, ax1 = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(7, 7))
-#ax1.set_axis_off()
-ax1.set_xlim3d(-env.radius*1.01, env.radius*1.01)
-ax1.set_ylim3d(-env.radius*1.01, env.radius*1.01)
-ax1.set_zlim3d(-env.radius*1.01, env.radius*1.01)
+if graph_on:
+    fig, ax1 = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(7, 7))
+    #ax1.set_axis_off()
+    ax1.set_xlim3d(-env.radius*1.01, env.radius*1.01)
+    ax1.set_ylim3d(-env.radius*1.01, env.radius*1.01)
+    ax1.set_zlim3d(-env.radius*1.01, env.radius*1.01)
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
 
-centre = [0,0,0]
-radius = env.radius
-theta = np.linspace(0, 2*np.pi, 100)
-x = centre[0] + radius * np.cos(theta)
-y = centre[1] + radius * np.sin(theta)
-#ax1.plot(x, y, c='black')
 
-'''
-scatter3D = ax1.scatter(np.array([pos[0] for pos in all_positions]),
-                        np.array([pos[1] for pos in all_positions]), 
-                        np.array([pos[2] for pos in all_positions]),
-                        s=10,
-                        c=['blue' if isinstance(agent, Prey) else 'red' for agent in pop.population_array])
-xyscatter = ax1.scatter(np.array([pos[0] for pos in all_positions]),
-                        np.array([pos[1] for pos in all_positions]), 
-                        np.full(pop.population_size,-env.radius), 
-                        zdir='z', s=10, c='gray', alpha=0.4)
-xzscatter = ax1.scatter(np.array([pos[0]for pos in all_positions]), 
-                        np.full(pop.population_size,env.radius), 
-                        np.array([pos[2] for pos in all_positions]), 
-                        zdir='y', s=10, c='gray', alpha=0.4)
-yzscatter = ax1.scatter(np.full(pop.population_size,-env.radius), 
-                        np.array([pos[1] for pos in all_positions]), 
-                        np.array([pos[2] for pos in all_positions]), 
-                        zdir='x', s=10, c='gray', alpha=0.4)'''
+    scatter3D = ax1.scatter(np.array([pos[0] for pos in all_positions]),
+                            np.array([pos[1] for pos in all_positions]), 
+                            np.array([pos[2] for pos in all_positions]),
+                            s=10,
+                            c=['blue' if isinstance(agent, Prey) else 'red' for agent in pop.population_array])
+    xyscatter = ax1.scatter(np.array([pos[0] for pos in all_positions]),
+                            np.array([pos[1] for pos in all_positions]), 
+                            np.full(pop.population_size,-env.radius), 
+                            zdir='z', s=10, c='gray', alpha=0.4)
+    xzscatter = ax1.scatter(np.array([pos[0]for pos in all_positions]), 
+                            np.full(pop.population_size,env.radius), 
+                            np.array([pos[2] for pos in all_positions]), 
+                            zdir='y', s=10, c='gray', alpha=0.4)
+    yzscatter = ax1.scatter(np.full(pop.population_size,-env.radius), 
+                            np.array([pos[1] for pos in all_positions]), 
+                            np.array([pos[2] for pos in all_positions]), 
+                            zdir='x', s=10, c='gray', alpha=0.4)
 
 for n in range(REPETITIONS):
-    pop = Population(population_size=POPULATION, number_of_neighbours=9, environment=env)
+    pop = Population(population_size=POPULATION, number_of_neighbours=NEIGHBOURS, environment=env)
     all_positions = pop.population_positions
-    data = DataRecorder(pop, TIMESTEPS, REPETITIONS)
+    
     for t in range(TIMESTEPS):            #Update the scatter plot for each timestep
         pop.update_positions(env)
-        data.update_data(time=t, repetitions=n)
-        '''
-        scatter3D._offsets3d = (np.array([pos[0] for pos in pop.population_positions]),
-                                np.array([pos[1] for pos in pop.population_positions]), 
-                                np.array([pos[2] for pos in pop.population_positions]))
-        xyscatter._offsets3d = (np.array([pos[0] for pos in pop.population_positions]), 
-                                np.array([pos[1] for pos in pop.population_positions]),
-                                np.full(pop.population_size,-env.radius))
-        xzscatter._offsets3d = (np.array([pos[0] for pos in pop.population_positions]),
-                                np.full(pop.population_size, env.radius),
-                                np.array([pos[2] for pos in pop.population_positions]))
-        yzscatter._offsets3d = (np.full(pop.population_size,-env.radius),
-                                np.array([pos[1] for pos in pop.population_positions]),
-                                np.array([pos[2] for pos in pop.population_positions]))
-        '''
-        plt.pause(0.001)
+        data_recorder.update_data(pop, time=t, repetitions=n)
+        print(f"Time: {t}, Repetition = {n}")
+        print(data_recorder.get_data()[1])
+        
+        if graph_on:
+            scatter3D._offsets3d = (np.array([pos[0] for pos in pop.population_positions]),
+                                    np.array([pos[1] for pos in pop.population_positions]), 
+                                    np.array([pos[2] for pos in pop.population_positions]))
+            xyscatter._offsets3d = (np.array([pos[0] for pos in pop.population_positions]), 
+                                    np.array([pos[1] for pos in pop.population_positions]),
+                                    np.full(pop.population_size,-env.radius))
+            xzscatter._offsets3d = (np.array([pos[0] for pos in pop.population_positions]),
+                                    np.full(pop.population_size, env.radius),
+                                    np.array([pos[2] for pos in pop.population_positions]))
+            yzscatter._offsets3d = (np.full(pop.population_size,-env.radius),
+                                    np.array([pos[1] for pos in pop.population_positions]),
+                                    np.array([pos[2] for pos in pop.population_positions]))
+            ax1.set_title(f'Time: {t}')
+            
+            plt.pause(0.001)
 
 
+polarisation_data = data_recorder.get_data()[1]
+rotation_data = data_recorder.get_data()[2]
+print(f"Polarisation data: {polarisation_data}")
 
-polarisation_data = data.get_data()[1]
-rotation_data = data.get_data()[2]
-
-for n in range(REPETITIONS):
-    fig, ax = plt.subplots()
-    time = np.linspace(0, TIMESTEPS, TIMESTEPS)
+time = np.linspace(0, TIMESTEPS, TIMESTEPS)
+for r in range(REPETITIONS):
+    fig, ax = plt.subplots(figsize=(5,4))
     ax.set_ylim(-0.05,1.05)
-    ax.plot(time, polarisation_data[n], label='Polarisation', c='red')
-    ax.plot(time, rotation_data[n], label='Rotation', c='blue')
+    ax.plot(time, polarisation_data[r], label='Polarisation', c='red')
+    ax.plot(time, rotation_data[r], label='Rotation', c='blue')
     ax.legend()
+    ax.set_title(f'Repetition {r+1}')
 
 plt.show()
