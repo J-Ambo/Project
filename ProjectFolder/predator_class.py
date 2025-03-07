a0b64f7ddaebae61757a0743f4354a089dd9bb5a
JAC <jamescrawley19@btinternet.com>
2025-03-07 11:23:53 +0000

    Added field of view for the predator. Working towards measuring predator success rate during its attack

--- ProjectFolder/predator_class.py
+++ ProjectFolder/predator_class.py
@@ -15,31 +15,35 @@ class Predator(Parent):
         self.minimum_distance_to_prey = None   #np.array([])
         self.fixed_direction = None
         self.skip = False
-        self.infront = True
+        self.fov = np.deg2rad(90)
+        self.previous_neighbours = np.array([])
 
     def find_neighbours(self, tree, population):
-        neighbours, distances = tree.query_radius([self.position], 20, return_distance=True)
-        neighbours = neighbours[distances!=0]
-        distances = distances[distances!=0]
-        if neighbours[distances!=0].size == 0:
-            print('early exit')
+        neighbours, distances = tree.query_radius([self.position], 8, return_distance=True)
+        self_mask = (distances[0] != 0)
+        
+        neighbours = neighbours[0][self_mask]
+        distances = distances[0][self_mask]
+
+        if neighbours.size == 0:
+            #print('early exit')
             return np.array([]), np.array([])
         
-        directions_to_neighbours = (neighbours - self.position)
+        neighbour_positions = population.all_positions[neighbours]
+        directions_to_neighbours = (neighbour_positions - self.position)
         np.divide(directions_to_neighbours, distances[:,np.newaxis], out=directions_to_neighbours)
 
-        dot_products = np.dot(directions_to_neighbours, neighbours)
+        dot_products = np.dot(directions_to_neighbours, self.direction)
         dot_products = np.round(dot_products, 3)
 
         angles_to_neighbours = np.arccos(dot_products)
-       # print(angles_to_neighbours)
-
-        mask = angles_to_neighbours <= self.half_perception_angle
+        mask = angles_to_neighbours <= self.fov/2
         visible_indices = neighbours[mask]
         visible_distances = distances[mask]
-        return neighbours[distances!=0], distances
 
-    def calculate_direction(self, tree, population):
+        return visible_indices, visible_distances      #population.all_positions[visible_indices]
+
+    def calculate_direction(self, population):
         vector_to_com = population.average_school_position - self.position
         distance_to_com = np.linalg.norm(vector_to_com) 
         
@@ -52,23 +56,39 @@ class Predator(Parent):
             self.fixed_direction = self.direction
    
     def fnc(self, tree, population):
-        neighbours, distances = self.find_neighbours(tree, population)
-       # print(neighbours)
+        neighbours_indices, distances = self.find_neighbours(tree, population)
+        
+        self.previous_neighbours = neighbours_indices
 
-        distances_to_prey = np.linalg.norm(population.inlier_positions - self.position, axis=1)
-        predator_school_vectors = (population.inlier_positions - self.position) / distances_to_prey[:,np.newaxis]
-        dot_products = np.dot(predator_school_vectors, self.direction)
-       
-        if np.any(dot_products < 0) and (self.infront == True) and not np.all(dot_products < 0):
-            self.minimum_distance_to_prey = min(distances_to_prey)
+        msk = distances<1
+       # print(msk, neighbours_indices, neighbours_indices[msk])
 
-        elif np.all(dot_products < 0) or self.infront == False:
-            self.infront = False
+        
+        #distances_to_prey = np.linalg.norm(neighbours_positions - self.position, axis=1)
+       # print('distances', distances)
+        if neighbours_indices.size == 0:
             self.minimum_distance_to_prey = None
+
+        else:
+            self.minimum_distance_to_prey = min(distances)
+
+        if neighbours_indices.size != 0:
+            print('PREVIOUS',self.previous_neighbours, 'CURRENT', neighbours_indices)
+            print('SAME',list(set(neighbours_indices).intersection(self.previous_neighbours)))
+
+        #predator_school_vectors = (neighbours_positions - self.position) / distances_to_prey[:,np.newaxis]
+        #dot_products = np.dot(predator_school_vectors, self.direction)
+       
+        #if np.any(dot_products < 0) and (self.infront == True) and not np.all(dot_products < 0):
+         #   self.minimum_distance_to_prey = min(distances_to_prey)
+
+        #elif np.all(dot_products < 0) or self.infront == False:
+         #   self.infront = False
+          #  self.minimum_distance_to_prey = None
 
     def update_predator(self, tree, population):
         self.fnc(tree, population)
-        self.calculate_direction(tree, population)
+        self.calculate_direction(population)
         if self.fixed_direction is not None:
             self.position += self.fixed_direction * self.speed
         else:
